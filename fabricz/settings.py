6afb2bd9ce1a908a312be5d9c856d452ecaf7f42
Yorick van den Hurk <nowane@gmail.com>
2022-01-26 11:41:38 +0000

    Deploy to heroku

--- fabricz/settings.py
+++ fabricz/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -27,7 +28,10 @@ SECRET_KEY = 'django-insecure-gnn=(^3=49(_4nmo0kl@st^$a8pnhsgv0vahtz6edugyy$r8kv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'fabricz.herokuapp.com',
+    'localhost',
+]
 
 
 # Application definition
@@ -109,12 +113,17 @@ WSGI_APPLICATION = 'fabricz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
