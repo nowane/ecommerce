0eaee87cb4695acf75cc1c27c6766da0cade856f
Yorick van den Hurk <nowane@gmail.com>
2022-02-04 10:56:32 +0000

    Revert back to int quantity

--- bag/views.py
+++ bag/views.py
@@ -12,7 +12,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
-    quantity = float(request.POST.get('quantity'))
+    quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
@@ -22,5 +22,5 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
+    print(bag)
     return redirect(redirect_url)
