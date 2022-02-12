8c39c61cfb08877609abe917e970d86ecf793961
Yorick van den Hurk <nowane@gmail.com>
2022-02-12 22:42:50 +0000

    Add bag overview to succes toast

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.shortcuts import (render, redirect, reverse, HttpResponse,
+                              get_object_or_404)
 from django.contrib import messages
 
 from products.models import Product
@@ -22,6 +23,8 @@ def add_to_bag(request, item_id):
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(
+            request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {product.name} to your bag')
@@ -33,13 +36,17 @@ def add_to_bag(request, item_id):
 def update_bag(request, item_id):
     """ Update the quantity of the specified product in the shopping bag """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(
+            request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -48,13 +55,16 @@ def update_bag(request, item_id):
 def remove_from_bag(request, item_id):
     """ Removes a product from the shopping bag """
 
+    product = get_object_or_404(Product, pk=item_id)
     bag = request.session.get('bag', {})
 
     try:
         bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
