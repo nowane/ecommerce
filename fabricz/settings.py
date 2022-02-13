cf67728c1dfe752c1f0f5c82b932d4de7205ef62
Yorick van den Hurk <nowane@gmail.com>
2022-02-13 14:35:45 +0000

    Add crispy forms and checkout view

--- fabricz/settings.py
+++ fabricz/settings.py
@@ -50,6 +50,8 @@ INSTALLED_APPS = [
     'checkout',
 
     'storages',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -63,6 +65,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'fabricz.urls'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 TEMPLATES = [
     {
@@ -81,6 +87,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
